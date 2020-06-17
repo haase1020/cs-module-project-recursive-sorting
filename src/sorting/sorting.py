4c523c549d57ee9e47e9d2de8fc913d14def75d7
Jon Haase <haase1020@gmail.com>
2020-06-16 20:13:42 -0400

    initial commit with psuedo code for merge sort problem

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -2,23 +2,36 @@
 def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
-
-    # Your code here
-
+    if len(arrA) or len(arrB) < 1
 
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
-def merge_sort(arr):
-    # Your code here
 
+
+def merge_sort(arr):
+    # split list into 2 halves
+    if len(arr) == 1
+    return arr
+    left_array = []
+    right_array = []
+    # further split up first half
+    # further split up until only 1 in each array for first array
+    # recursively merge adjacent partitions
+    # for i = leftpart_index to rightpart_index
+    # if leftpart_headvalue <= rightpart_headvalue
+    # copy leftpart_headvalue
+    # else: copy rightpart_headvalue
+    # copy elements back to the original array
 
     return arr
 
-# STRETCH: implement the recursive logic for merge sort in a way that doesn't 
+# STRETCH: implement the recursive logic for merge sort in a way that doesn't
 # utilize any extra memory
-# In other words, your implementation should not allocate any additional lists 
+# In other words, your implementation should not allocate any additional lists
 # or data structures; it can only re-use the memory it was given as input
+
+
 def merge_in_place(arr, start, mid, end):
     # Your code here
 
@@ -26,3 +39,15 @@ def merge_in_place(arr, start, mid, end):
 def merge_sort_in_place(arr, l, r):
     # Your code here
 
+    # from python quick sort algorithm video: https://www.youtube.com/watch?v=CB_NCoxzQnk
+
+
+def quick_sort(A):
+    qick_sort2(A, 0, len(A) - 1)
+
+
+def quick_sort2(A, low, hi):
+    if low < hi:
+        p = partition(A, low, hi)
+        quick_sort2(A, low, p-1
+                    quick_sort2(A, p + 1, hi))
