0faa83a9686ff671b7e9e35a696c9ea17d6ad49e
Jon Haase <haase1020@gmail.com>
2020-06-17 05:27:04 -0400

    complete binary search and merge sort but tests are not passing

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,30 +1,53 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
-def merge(arrA, arrB):
-    elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements
-    if len(arrA) or len(arrB) < 1
+# def merge(arrA, arrB):
+#     elements = len(arrA) + len(arrB)
+#     merged_arr = [0] * elements
+#     if len(arrA) or len(arrB) < 1
 
-    return merged_arr
+#     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
 
 
-def merge_sort(arr):
-    # split list into 2 halves
-    if len(arr) == 1
-    return arr
-    left_array = []
-    right_array = []
-    # further split up first half
-    # further split up until only 1 in each array for first array
-    # recursively merge adjacent partitions
-    # for i = leftpart_index to rightpart_index
-    # if leftpart_headvalue <= rightpart_headvalue
-    # copy leftpart_headvalue
-    # else: copy rightpart_headvalue
-    # copy elements back to the original array
+def merge_sort(mainlist):
+    print("split list", mainlist)
+    # base case
 
-    return arr
+    if len(mainlist) > 1:
+        mid = len(mainlist)//2
+        lefthalf = mainlist[:mid]
+        righthalf = mainlist[mid:]
+
+        merge_sort(lefthalf)
+        merge_sort(righthalf)
+
+        a = 0  # lefthalf starting index
+        b = 0  # righthalf starting index
+        c = 0  # mainlist starting index
+        while a < len(lefthalf) and b < len(righthalf):
+            if lefthalf[a] <= righthalf[b]:
+                mainlist[c] = lefthalf[a]
+                a = a + 1
+            else:
+                mainlist[c] = righthalf[b]
+                b = b + 1
+            c = c + 1
+
+        while a < len(lefthalf):
+            mainlist[c] = lefthalf[a]
+            a = a + 1
+            c = c + 1
+        while b < len(righthalf):
+            mainlist[c] = righthalf[b]
+            b = b + 1
+            c = c + 1
+    print("merging to main list", mainlist)
+
+
+mainlist = [5, 7, 2, 3, 9, 1, 4, 0]
+merge_sort(mainlist)
+print(mainlist)
+
 
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't
 # utilize any extra memory
@@ -32,22 +55,22 @@ def merge_sort(arr):
 # or data structures; it can only re-use the memory it was given as input
 
 
-def merge_in_place(arr, start, mid, end):
-    # Your code here
+# def merge_in_place(arr, start, mid, end):
+#     # Your code here
 
 
-def merge_sort_in_place(arr, l, r):
-    # Your code here
+# def merge_sort_in_place(arr, l, r):
+#     # Your code here
 
-    # from python quick sort algorithm video: https://www.youtube.com/watch?v=CB_NCoxzQnk
-
-
-def quick_sort(A):
-    qick_sort2(A, 0, len(A) - 1)
+#     # from python quick sort algorithm video: https://www.youtube.com/watch?v=CB_NCoxzQnk
 
 
-def quick_sort2(A, low, hi):
-    if low < hi:
-        p = partition(A, low, hi)
-        quick_sort2(A, low, p-1
-                    quick_sort2(A, p + 1, hi))
+# def quick_sort(A):
+#     qick_sort2(A, 0, len(A) - 1)
+
+
+# def quick_sort2(A, low, hi):
+#     if low < hi:
+#         p = partition(A, low, hi)
+#         quick_sort2(A, low, p-1
+#                     quick_sort2(A, p + 1, hi))
--- src/sorting/test_sorting.py
+++ src/sorting/test_sorting.py
@@ -1,6 +1,8 @@
+
 import unittest
 import random
 from sorting import *
+
 
 class RecursiveSortingTests(unittest.TestCase):
     def test_merge_sort(self):
@@ -30,7 +32,7 @@ class RecursiveSortingTests(unittest.TestCase):
 
     #     merge_sort_in_place(arr2, 0, len(arr2)-1)
     #     self.assertEqual(arr2, [])
-        
+
     #     merge_sort_in_place(arr3, 0, len(arr3)-1)
     #     self.assertEqual(arr3, [2])
 
